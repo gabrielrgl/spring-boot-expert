66da0d1e3a0fc80b362a8d7d7f78e09573074513
Gabriel Rangel <gabrielrglpro@gmail.com>
2024-04-28 22:24:27 -0300

    configurando properties de desenvolvimento e producao

--- src/main/java/io/github/gabrielrglpro/MinhaConfiguration.java
+++ src/main/java/io/github/gabrielrglpro/MinhaConfiguration.java
@@ -1,9 +1,21 @@
 package io.github.gabrielrglpro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.xml.ws.WebEndpoint;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
+
+    @Bean()
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 
 }
