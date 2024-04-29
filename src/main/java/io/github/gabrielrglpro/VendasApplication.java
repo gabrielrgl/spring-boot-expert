006162505d2b02046cec2478e10ef975ac48d6cc
Gabriel Rangel <gabrielrglpro@gmail.com>
2024-04-28 21:59:05 -0300

    injecao de dependencia e application.properties

--- src/main/java/io/github/gabrielrglpro/VendasApplication.java
+++ src/main/java/io/github/gabrielrglpro/VendasApplication.java
@@ -2,20 +2,20 @@ package io.github.gabrielrglpro;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication()
 public class VendasApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("/hello")
---
+++ src/main/java/io/github/gabrielrglpro/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package io.github.gabrielrglpro.repository;
+
+import io.github.gabrielrglpro.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    public void persistir(Cliente cliente) {
+
+    }
+}
